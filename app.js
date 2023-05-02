0d5a0ad3a4fde26f7efc6220c9432476fff0851c
mishraom12345 <mishrahariom525@gmail.com>
2023-05-02 07:58:44 +0530

    usingexpress handle incoming request replace node

--- app.js
+++ app.js
@@ -1,14 +1,23 @@
-
 const express = require('express')
+const bodyParser = require('body-parser')
 
 const app = express()
 
-app.use((req,res,next)=>{
-    console.log("in a middleware")
-    next()// allow request to continue to next middle ware in line 
+app.use(bodyParser.urlencoded({extended:false}))
+
+app.use('/add-product',(req,res,next)=>{
+    console.log("in another middleware")
+    res.send(`<form action = "/product" method = "POST"> <input type = "text" name = "title"><input type = "number" name = "quantity"><button type = "submit"> Add Product </button></form>`)
+    
 })
 
-app.use((req,res,next)=>{
+app.use('/product',(req,res,next)=>{
+    console.log(req.body)
+    res.redirect('/')
+})
+
+
+app.use('/',(req,res,next)=>{
     console.log("in another middleware")
     res.send("<h1>hello from express js</h1>")
 })
